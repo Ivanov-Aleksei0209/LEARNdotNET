6bc2cb8f7af60f0c5e11c11769f25f37827fab3b
ivanovaleksei <ivanov.aleksei020979@gmail.com>
2023-06-22 21:06:10 +0300

    is all wrong

---
+++ MigrationsExamples/MigrationsExamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MigrationsExamples
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
