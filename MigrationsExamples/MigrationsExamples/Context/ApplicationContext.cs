a17137fc1fe226bc436a9ec303fba9a2252d493f
ivanovaleksei <ivanov.aleksei020979@gmail.com>
2023-07-19 15:43:04 +0300

    new project created. Migration and DataBase automatically created.

---
+++ MigrationsExamples/MigrationsExamples/Context/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MigrationsExamples.Models;
+
+namespace MigrationsExamples.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Blog> Blog { get; set; }
+
+    }
+}
---
+++ MigrationsExamples/MigrationsExamples/Models/Blog.cs
@@ -0,0 +1,12 @@
+﻿namespace MigrationsExamples.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }  
+
+
+    }
+}
